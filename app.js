a66019dcd4297f0e6c4457b7246befb6ec472efa
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-29 21:35:15 +0530

    Uninerry operators

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ let a = 10;
 
 let b = 5;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
-console.log(a**b);
+console.log(a++);
+console.log(++a);
+console.log(a);
+
+console.log(b);
+console.log(b++);
+console.log(b);
+console.log(++b)
+
+console.log(a--);
+console.log(--a);
+console.log(a);
