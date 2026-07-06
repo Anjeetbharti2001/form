ad4c045570ab31d8411a28fc7f0ce14b6d1116fa
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-06 17:14:14 +0530

    Random number guess access

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const max = prompt("Enter the max number ");
  
 const random = Math.floor(Math.random() * max) + 1;
-let 
+
+let guess = prompt("guess the number ");
+
+while(true){
+    if(guess == "quit"){
+        console.log("user quit");
+    }
+    if(guess == random){
+        console.log("you are right! congrates! random number was", random);
+        break;
+    }else{
+      guess = prompt("your guess was wrong . please try again ");
+    }
+}
+// guess the random number 
