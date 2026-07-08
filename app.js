ebdf0aee370e667db3b056b626898a67214adc2d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-08 07:59:54 +0530

    using the html, css and javasripts build a digital clock

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const max = prompt("Enter the max number ");
+// const max = prompt("Enter the max number ");
  
-const random = Math.floor(Math.random() * max) + 1;
+// const random = Math.floor(Math.random() * max) + 1;
 
-let guess = prompt("guess the number ");
+// let guess = prompt("guess the number ");
 
-while(true){
-    if(guess == "quit"){
-        console.log("user quit");
-    }
-    if(guess == random){
-        console.log("you are right! congrates! random number was", random);
-        break;
-    }if(guess == random){
-        console.log("You are right!! random number was", random);
-    }else if(guess < random){
-        guess = prompt("your guess was too small .please try again ")
-    }else{
-        guess = prompt("your guess was too large. please try again ")
-    }
-}
-// guess the random number 
+// while(true){
+//     if(guess == "quit"){
+//         console.log("user quit");
+//     }
+//     if(guess == random){
+//         console.log("you are right! congrates! random number was", random);
+//         break;
+//     }if(guess == random){
+//         console.log("You are right!! random number was", random);
+//     }else if(guess < random){
+//         guess = prompt("your guess was too small .please try again ")
+//     }else{
+//         guess = prompt("your guess was too large. please try again ")
+//     }
+// }
+// // guess the random number 
---
+++ time.js
@@ -0,0 +1,8 @@
+const now = new Date();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+
+console.log(now);
+console.log(hour, minutes, seconds);
