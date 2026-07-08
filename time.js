b6f1283e700cd6f04937924a1370230730cec459
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-08 08:37:46 +0530

    add function updatClock and SetInterval

--- time.js
+++ time.js
@@ -1,8 +1,19 @@
+function updateClock(){
+
+
+
 const now = new Date();
 let hour = now.getHours();
 let minutes = now.getMinutes();
 let seconds = now.getSeconds();
 
+const clock = document.querySelector(".clock");
+
+clock.innerText = `${hour}:${minutes}:${seconds}`;
 
 console.log(now);
 console.log(hour, minutes, seconds);
+}
+
+updateClock();
+setInterval(updateClock, 1000);
